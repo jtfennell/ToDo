317ea15c7beb3809ccbb780fb53be9f9430093cf
Jeff <jfennell@lion.lmu.edu>
2015-06-10 19:06:46 -0700

    create new activity for making a new task and add EditText views

--- app/src/main/java/com/jeff_fennell/todo/TaskContract.java
+++ app/src/main/java/com/jeff_fennell/todo/TaskContract.java
@@ -1,7 +1,20 @@
 package com.jeff_fennell.todo;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by jeff on 6/9/15.
  */
-public class TaskContract {
+public final class TaskContract {
+
+    //empty constructor to prevent instantiation
+    public TaskContract(){}
+
+    public static abstract class TaskEntry implements BaseColumns {
+        public static final String TABLE_NAME = "task";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_DETAILS = "details";
+        public static final String COLUMN_NAME_COMPLETE = "complete";
+
+    }
 }
--- app/src/main/java/com/jeff_fennell/todo/TaskDbHelper.java
+++ app/src/main/java/com/jeff_fennell/todo/TaskDbHelper.java
@@ -1,7 +1,37 @@
 package com.jeff_fennell.todo;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by jeff on 6/9/15.
  */
-public class TaskDbHelper {
+public class TaskDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Task.db";
+    //private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS" + TABLE_NAME;
+    private static final String SQL_CREATE_ENTRIES ="";
+
+    public TaskDbHelper(Context context) {
+        super(context,DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    //look at options for implementing this method. current implementation copied from
+    //http://developer.android.com/training/basics/data-storage/databases.html
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+//    @Override
+//    public void onOpen(){
+//
+//    }
 }
